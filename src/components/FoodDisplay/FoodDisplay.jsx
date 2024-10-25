bb89b425db1bb812696ddcfc452010d5d8cd7a76
Augusto <augusto.perales.g@uni.pe>
2024-10-25 09:19:53 -0500

    foodDisplay

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -12,8 +12,8 @@ const FoodDisplay = ({category}) => {
       <h2>Los mejores platos cerca tuyo!</h2>
       <div className='food-display-list'>
         {food_list.map((item)=>{
-          if (category==="All" || category===item.food_category) {
-            return <FoodItem key={item.food_id} image={item.food_image} name={item.food_name} desc={item.food_desc} price={item.food_price} id={item.food_id}/>
+          if (category==="All" || category===item.category) {
+            return <FoodItem key={item.id} image={item.image} name={item.name} desc={item.description} price={item.price} id={item.id}/>
           }
         })}
       </div>
